c7f3c18401f5efbf30115e775588ba04422133f2
Louis Chavez <67764701+chavez62@users.noreply.github.com>
2024-10-02 09:37:17 -0700

    Update script.js

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ document.getElementById('getWeatherBtn').addEventListener('click', function () {
 
     if (city) {
         // Fetch weather data from your API
-        fetch(`https://weatherappapi-a9f9c4bxaggfhreh.westus-01.azurewebsites.net/api/weather/${city}`)
+        fetch(`https://weatherforecastapi-g5aygdb8aafmbcg2.westus-01.azurewebsites.net/api/weather/${city}`)
             .then(response => response.json())
             .then(data => {
                 // Check if data is valid
